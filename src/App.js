4efe1f2f3b78ed3da85dc17c10c2178e9a5d1b8a
loganwong52 <loganwong52@gmail.com>
2022-07-11 14:29:46 -0700

    completed release 1

--- src/App.js
+++ src/App.js
@@ -13,12 +13,19 @@ function App() {
   }, [])
 
   // helpers
+  // The Mole objects in dens change their visability every 1.5 seconds
+  // This method is called in useEffect.
+  // But WHEN is useEffect called? 
+  // I think... it's called automatically the moment the browser is loaded or reloaded
   function startGame() {
     setInterval(() => {
       setDens(getDensState())
     }, 1500)
   }
 
+  // Uses map() to loop thru the dens array and gives it an isMoleVisible attribute
+  // that's either true or false. Used in Mole.js to determine whether to render it
+  // or not.
   function getDensState() {
     return new Array(9).fill({}).map(() => {
       return {
@@ -27,6 +34,9 @@ function App() {
     })
   }
 
+  // Passed into Mole.js. For the img tag, it has an onClick={} attribute.
+  // When the image is clicked, points is incremented by 1.
+  // The Image currently doesn't disappear
   function onMoleWhacked() {
     setPoints(points + 1)
   }
@@ -34,7 +44,7 @@ function App() {
   // renders
   const denElements = dens.map((den, index) => {
     return (
-      <Mole key={`mole-${index}`} den={den} />
+      <Mole key={`mole-${index}`} den={den} onMoleWhacked={onMoleWhacked} />
     )
   })
 
--- src/components/mole/Mole.js
+++ src/components/mole/Mole.js
@@ -3,12 +3,12 @@ import React from 'react'
 import MoleIcon from './Mole.svg'
 
 function Mole(props) {
-  // const { dens, setDens } = props; // destructuring 
+  const { den, onMoleWhacked } = props; // destructuring 
 
   return (
     <div className="den">
-      {props.den.isMoleVisible &&
-        <img src={MoleIcon} className="Mole" alt="Mole" />}
+      {den.isMoleVisible &&
+        <img src={MoleIcon} className="Mole" alt="Mole" onClick={onMoleWhacked} />}
 
     </div>
   )
